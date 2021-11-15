4a9dcd1eb9436edb9c61c0cc0f66087d012a821f
Andrea <aef2expitdet2020@gmail.com>
2021-11-15 08:38:35 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-# Exercise One
-birth_month = input("Enter your birth month: ")
-print(birth_month)
+adjective = input("Enter an adjective:") 
+noun = input("Enter a noun:")
+past_tense_verb = input("Enter a past_tense_verb:")
+adverb = input("Enter an adverb:")
+adjective1 = input("Enter an adjective1:")
+noun1 = input("Enter a noun1:")
+noun2 = input("Enter a noun2:") 
+adjective2 = input("Enter an adjective2:")
+verb = input("Enter a verb:")
+adverb2 = input("Enter an adverb2:")
+past_tense_verb2 = input("Enter a past_tense_verb2:")
+adjective3 = input("Enter an adjective3:")
 
-# Challenge
-user_name = input("Enter name: ")
-print(f"{user_name}, your birth month is {birth_month}.")
+
+print("Today I went to the zoo. I saw a" + adjective) 
+print(noun +"jumping up and down in its tree.") 
+print("He" + past_tense_verb)
+print(adverb + "through the large tunnel that led to its " + adjective1) 
+print(noun1 + "I got some peanuts and passed them through the cage to a gigantic gray ")
+print(noun2 + "towering above my head. ") 
+print("Feeding that animal made me hungry. I went to get a" + adjective2)
+print("scoop of ice cream. It filled my stomach. ") 
+print("Afterwards I had to" + verb) 
+print(adverb + "to catch our bus. ") 
+print("When I got home I " + past_tense_verb2)
+print("my mom for a " + adjective3)
+print("day at the zoo.")
+
+
+
+
+
+
